2f9459f1e1a36539be875463b29cf27a33182f4d
Manraj Singh <manrajsinghgrover@gmail.com>
2016-06-16 19:10:51 +0530

    Class created | Constants added

---
+++ constants.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Manraj Singh
+* @Date:   2016-06-16 18:44:04
+* @Last Modified by:   Manraj Singh
+* @Last Modified time: 2016-06-16 18:56:54
+*/
+
+'use strict';
+
+export const RUN_URL = 'http://api.hackerrank.com/checker/submission.json';
+export const LANG_URL = 'http://api.hackerrank.com/checker/languages.json';
--- index.js
+++ index.js
@@ -2,8 +2,30 @@
 * @Author: Manraj Singh
 * @Date:   2016-06-15 21:53:33
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-06-15 23:07:25
+* @Last Modified time: 2016-06-16 19:09:46
 */
 
 'use strict';
 
+
+import * as constants from './constants'
+
+class HackerRank {
+  constructor(apiKey) {
+    this._runURL = constants.RUN_URL;
+    this._langURL = constants.LANG_URL;
+    this._apiKey = apiKey;
+  }
+  
+  get runURL() {
+    return this._runURL;
+  }
+
+  get langURL() {
+    return this._langURL;
+  }
+
+  get apiKey() {
+    return this._apiKey;
+  }
+}
