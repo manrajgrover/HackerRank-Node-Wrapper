4116685ceea8cfef66528b20ce1f8aee7a5a50c8
Manraj Singh <manrajsinghgrover@gmail.com>
2016-06-18 18:34:46 +0530

    Example Added

---
+++ Example/index.js
@@ -0,0 +1,35 @@
+/*
+* @Author: Manraj Singh
+* @Date:   2016-06-18 18:33:31
+* @Last Modified by:   Manraj Singh
+* @Last Modified time: 2016-06-18 18:34:26
+*/
+
+'use strict';
+import HackerRank from 'hackerrank-node-wrapper';
+
+var hr = new HackerRank('yourApiKey');
+
+hr.getLanguages(function(error, response){
+	if(error){
+		console.log("Error: ", error);
+	}
+	else{
+		console.log(response.body);
+	}
+});
+
+var data = {
+  'source': 'print "Hello World"',
+  'lang': 5,
+  'testcases': '["1"]'
+};
+
+hr.run(data,function(error, response){
+	if(error){
+		console.log("Error: "+ error);
+	}
+	else{
+		console.log(response.body);
+	}
+})
