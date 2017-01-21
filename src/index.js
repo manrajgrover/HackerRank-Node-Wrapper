1ce1d90beb0fd376bf4e8d79cbf330cee650139a
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-01-21 22:56:13 +0530

    Remove: File headers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-/*
-* @Author: Manraj Singh
-* @Date:   2016-06-15 21:53:33
-* @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-06-18 20:00:12
-*/
-
 'use strict';
 
 import * as constants from './constants';
