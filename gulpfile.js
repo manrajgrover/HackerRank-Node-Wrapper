f0e86265428f8221cb30486d19ec08562d7c21f4
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-01-24 21:54:16 +0530

    Remove: File headers

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,3 @@
-/*
-* @Author: Manraj Singh
-* @Date:   2016-06-18 01:42:54
-* @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-06-30 03:09:12
-*/
-
 var gulp = require('gulp');
 var browserify = require("browserify");
 var babelify = require("babelify");
--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-/*
-* @Author: Manraj Singh
-* @Date:   2016-06-18 20:00:34
-* @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-06-18 20:17:11
-*/
-
 module.exports = require('./dist/index');
