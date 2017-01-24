6b5a32f446bf0ce8219e26a9ec6357a21bbca4c0
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-01-24 22:04:48 +0530

    Fix: Linting errors in Example/index.js

--- Example/index.js
+++ Example/index.js
@@ -1,35 +1,25 @@
-/*
-* @Author: Manraj Singh
-* @Date:   2016-06-18 18:33:31
-* @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-06-18 20:01:49
-*/
+const HackerRank = require('hackerrank-node-wrapper');
 
-'use strict';
-import HackerRank from 'hackerrank-node-wrapper';
+const hr = new HackerRank('yourApiKey');
 
-var hr = new HackerRank('yourApiKey');
-
-hr.getLanguages(function(error, response){
-  if(error){
-    console.log("Error: ", error);
-  }
-  else{
+hr.getLanguages((error, response) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
     console.log(response.body);
   }
 });
 
-var data = {
-  'source': 'print "Hello World"',
-  'lang': 5,
-  'testcases': '["1"]'
+const data = {
+  source: 'print "Hello World"',
+  lang: 5,
+  testcases: '["1"]',
 };
 
-hr.run(data,function(error, response){
-  if(error){
-    console.log("Error: "+ error);
-  }
-  else{
+hr.run(data, (error, response) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else {
     console.log(response.body);
   }
 });
