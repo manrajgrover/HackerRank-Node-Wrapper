b065d6ddfba0073378cc0e285b60b6958c481815
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-01-24 21:58:25 +0530

    Fix: ESLint errors in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-var browserify = require("browserify");
-var babelify = require("babelify");
-var source = require('vinyl-source-stream');
-var gutil = require('gulp-util');
-var babel = require('gulp-babel');
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const gutil = require('gulp-util');
+const babel = require('gulp-babel');
 
 
 gulp.task('browser', () => {
   browserify({ debug: true })
     .transform(babelify)
-    .require("./Example/test.js", { entry: true })
+    .require('./Example/test.js', { entry: true })
     .bundle()
-    .on('error',gutil.log)
+    .on('error', gutil.log)
     .pipe(source('app.js'))
     .pipe(gulp.dest('./Example/source'));
 });
@@ -19,13 +19,13 @@ gulp.task('browser', () => {
 gulp.task('es6', () => {
   gulp.src('src/*.js')
     .pipe(babel({
-      presets: ['es2015']
+      presets: ['es2015'],
     }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['./src/*.js'],['browser', 'es6'])
+  gulp.watch(['./src/*.js'], ['browser', 'es6']);
 });
- 
+
 gulp.task('default', ['browser', 'es6', 'watch']);
