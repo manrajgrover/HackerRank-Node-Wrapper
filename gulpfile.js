1c1bc59280dce6a44c2883d832e5a15d3f3c3f7b
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-11 22:42:36 +0530

    Gulpfile: Remove unwanted tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,5 @@
 const gulp = require('gulp');
-const browserify = require('browserify');
-const babelify = require('babelify');
-const source = require('vinyl-source-stream');
-const gutil = require('gulp-util');
 const babel = require('gulp-babel');
-
-
-gulp.task('browser', () => {
-  browserify({ debug: true })
-    .transform(babelify)
-    .require('./Example/test.js', { entry: true })
-    .bundle()
-    .on('error', gutil.log)
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./Example/source'));
-});
 
 gulp.task('es6', () => {
   gulp.src('src/*.js')
@@ -25,7 +10,7 @@ gulp.task('es6', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch(['./src/*.js'], ['browser', 'es6']);
+  gulp.watch(['./src/*.js'], ['es6']);
 });
 
-gulp.task('default', ['browser', 'es6', 'watch']);
+gulp.task('default', ['es6']);
